e038162c4869d271f958473dfa0646999b1d85c2
TejasSuryavanshi <tejassuryavanshi06@gmail.com>
2025-07-22 22:00:28 +0530

    force dynamic rendering for about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,9 @@
+// When I deployed the project to vercel, the loading.tsx spinner didn't show up.
+// This happens beacuse Next.js automatically optimized the about page as static no SSR , so the loading.tsx file wasn't triggered.
+// I added the line below to force Next.js to render the page dynamically on every request. 
+
+export const dynamic = "force-dynamic";
+
 const About = async() =>{
     await new Promise((resolve)=>setTimeout(resolve,3000))
     return (
